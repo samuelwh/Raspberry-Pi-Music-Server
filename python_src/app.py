f1a8f3a67ce7aebf2f2a8a45562f0944aed2ec55
Samuelwh4110 <samuelwh6201@gmail.com>
2018-02-16 14:58:55 -0700

    additional changes and practice runs

--- python_src/app.py
+++ python_src/app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return render_template('j2_query.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    _username = request.form['username']
+    if _username:
+        return render_template('j2_response.html', username=_username)
+    else:
+        return 'Please go back and enter a username...',404
+
+
+
+
+
+
+'''
 @app.route("/")
 def index():
     return 'sneaky snake'
@@ -13,6 +31,6 @@ def test1():
 @app.route('/profile/<username>')
 def profile(username):
     return render_template("profile.html", name=username)
-
+'''
 if __name__ == "__main__":
     app.run(debug=True)
